4c42090d10a4118ffffb4e65df8e6ab7f5a4dc3d
Chester Lynn <chester.htooaunglinn@gmail.com>
2020-12-17 14:53:10 +0630

    Done home page and at checkout

---
+++ src/Home.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt="banner"
+        ></img>
+
+        <div className="home__row">
+          <Product
+            title="The Lean Start Up, a book that makes you wanna regret every shitty decision u've done in ur lift"
+            price={29.99}
+            image="http://pngimg.com/uploads/book/book_PNG51112.png"
+            rating={5}
+          />
+          <Product
+            title="Apple's latest earphones that are actually wireless but they throw in a wire to connect those so u wont lose them"
+            price={199.99}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/21zodo7QkUL._AC_SY200_.jpg"
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            title="Connect4, toys for kids"
+            price={4.99}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/91ldZZwCMqL._AC_SY400_.jpg"
+          />
+          <Product
+            title="Jenga for kids or adults or whoever"
+            price={4.99}
+            rating={2}
+            image="https://m.media-amazon.com/images/I/81OAWwX3djL._AC_UL640_FMwebp_QL65_.jpg"
+          />
+          <Product
+            title="VR glasses so you can escape reality and forget u r a complete horseshit"
+            price={299.99}
+            rating={5}
+            image="https://m.media-amazon.com/images/I/71URNvzoWqL._AC_UY436_FMwebp_QL65_.jpg"
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            title="Gigantic samsung monitor that nobody needs but rich people are gonna buy it"
+            price={1099.99}
+            rating={5}
+            image="https://m.media-amazon.com/images/I/91-hWcNu2yL._AC_UY436_FMwebp_QL65_.jpg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAPgEfVC0tVMQPhQwA6OHPoTFKMjGAEoMk",
+  authDomain: "clone-45d27.firebaseapp.com",
+  projectId: "clone-45d27",
+  storageBucket: "clone-45d27.appspot.com",
+  messagingSenderId: "83792495567",
+  appId: "1:83792495567:web:46a274f3c7671a29e7c8ba",
+  measurementId: "G-HYRVL1W0P2",
+};
