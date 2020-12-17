79c6a6a79350f51df438c3cb8f5173b108d1471c
Chester Lynn <chester.htooaunglinn@gmail.com>
2020-12-18 00:10:11 +0630

    add subtotal functionality

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+
 
 function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const getBasketTotal = (basket) => {
+    return basket?.reduce((amount, item) => {
+      return item.price + amount;
+    }, 0);
+  };
+  
   return (
     <div className="subtotal">
       <CurrencyFormat
@@ -10,7 +20,7 @@ function Subtotal() {
           return (
             <>
               <p>
-                Subtotal (0 items) : <strong>0</strong>
+                Subtotal ({basket?.length} items) : <strong>{value}</strong>
               </p>
               <small className="subtotal__gift">
                 <input type="checkbox" />
@@ -20,7 +30,7 @@ function Subtotal() {
           );
         }}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
